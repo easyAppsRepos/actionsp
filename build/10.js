ee9c3402eb3081adbe513609354b333eabd530ac
easyAppsRepos <jralfarog@gmail.com>
2018-04-08 07:18:02 -0600

    scanEDnddd

--- build/10.js
+++ build/10.js
@@ -98,7 +98,7 @@ var EscanerPage = (function () {
                     console.log(data);
                     //console.log(data.url);
                     // var youtubeVideo = data.url.split("v=")[1];
-                    var youtubeVideo = data.split('video_id=')[1].split('\\')[0];
+                    var youtubeVideo = data.split('href="https://www.youtube.com/watch?v=')[1].split('">')[0];
                     //YoutubeVideoPlayer.openVideo(youtubeVideo, function(result) { console.log('YoutubeVideoPlayer result = ' + result); console.log(result);});
                     me.goVideo(youtubeVideo);
                 }
