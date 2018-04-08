798dc4326e1dab0d604502386d469b292804f813
easyAppsRepos <jralfarog@gmail.com>
2018-04-08 16:07:20 -0600

    scanEDndddadddddd

--- build/0.js
+++ build/0.js
@@ -45,6 +45,7 @@ var YoutubevidePageModule = (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return YoutubevidePage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(29);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_platform_browser__ = __webpack_require__(26);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -56,6 +57,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 
 
+
 /**
  * Generated class for the YoutubevidePage page.
  *
@@ -63,22 +65,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
  * Ionic pages and navigation.
  */
 var YoutubevidePage = (function () {
-    function YoutubevidePage(navCtrl, navParams) {
+    function YoutubevidePage(navCtrl, navParams, sanitizer) {
         this.navCtrl = navCtrl;
         this.navParams = navParams;
+        this.sanitizer = sanitizer;
+        this.video = {};
     }
     YoutubevidePage.prototype.ionViewDidLoad = function () {
-        console.log(navParams.get('id'));
-        console.log(navParams.get('snippet'));
+        console.log(this.navParams.get('id'));
+        this.video.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.navParams.get('id'));
+        this.video.data = this.navParams.get('snippet');
+        console.log(this.navParams.get('snippet'));
         console.log('ionViewDidLoad YoutubevidePage');
     };
     YoutubevidePage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
             selector: 'page-youtubevide',template:/*ion-inline-start:"/Users/jose/Documents/appGym/myApp/src/pages/youtubevide/youtubevide.html"*/'<!--\n  Generated template for the YoutubevidePage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n  <ion-navbar>\n    <img style=\'    height: 30px; float: right;margin-right: 15px;\' src="assets/imgs/logoAmarillo.png"> \n  </ion-navbar>\n</ion-header>\n\n\n<ion-content    style=\'background-color: #c3d7e6\' >\n\n\n<div class=\'tituloHome\' style="background-color:#868686 !important;text-align:center;">INSTRUCCIONES DE USO</div>\n\n<div padding>\n\n  \n  <iframe width="100%" height="300px" [src]="video.url"  frameborder="30" allowfullscreen></iframe>\n\n  <p style="color: #444;">{{video.data?.title || ""}}</p>\n</div>\n\n</ion-content>\n'/*ion-inline-end:"/Users/jose/Documents/appGym/myApp/src/pages/youtubevide/youtubevide.html"*/,
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["k" /* NavParams */]])
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* NavController */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["k" /* NavParams */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["k" /* NavParams */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_2__angular_platform_browser__["c" /* DomSanitizer */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__angular_platform_browser__["c" /* DomSanitizer */]) === "function" && _c || Object])
     ], YoutubevidePage);
     return YoutubevidePage;
+    var _a, _b, _c;
 }());
 
 //# sourceMappingURL=youtubevide.js.map
